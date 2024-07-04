efa6262817d9ba00a7972412ebbe94ec9e97a921
ngothanhminh2004 <shinichikudo1423@gmail.com>
2024-07-04 23:29:13 +0700

    make a minor adjustment

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import movieRouter from "./routes/movies";
 import orderFood from "./routes/orderFood"
 import orderTicketRouter from "./routes/orderTicket";
 import bodyParser from "body-parser";
+import cors from 'cors'
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ const port = process.env.PORT || 3000;
 configViewEngine(app)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors())
 
 app.use('/', initRouter)
 
